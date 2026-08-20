a6823e6a5fc3ec1e66268a6840a50be273e1bc64
r4ppz <johnreyrabosa.f@gmail.com>
2026-08-20 17:33:06 +0800

    refactor: make it more easier to read

--- main.go
+++ main.go
@@ -10,72 +10,65 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
+type chatConfig struct {
+	execPath           string
+	headless           bool
+	userDataDir        string
+	profileDir         string
+	appURL             string
+	enableAutomation   bool
+	textBoxSelector    string
+	sendButtonSelector string
+	message            string
+}
+
+func defaultConfig() chatConfig {
+	return chatConfig{
+		execPath:           "/usr/bin/brave",
+		headless:           false,
+		userDataDir:        "/home/r4ppz/.config/BraveSoftware/Brave-Browser",
+		profileDir:         "Default",
+		appURL:             "https://chatgpt.com/?temporary-chat=true",
+		enableAutomation:   false,
+		textBoxSelector:    `[contenteditable="true"][role="textbox"]`,
+		sendButtonSelector: `button[data-testid="send-button"]`,
+		message:            "Hello, testing!",
+	}
+}
+
+func buildAllocatorOpts(cfg chatConfig) []chromedp.ExecAllocatorOption {
+	return append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.ExecPath(cfg.execPath),
+		chromedp.Flag("headless", cfg.headless),
+		chromedp.Flag("user-data-dir", cfg.userDataDir),
+		chromedp.Flag("profile-directory", cfg.profileDir),
+		chromedp.Flag("app", cfg.appURL),
+		chromedp.Flag("enable-automation", cfg.enableAutomation),
+	)
+}
+
+func runAutomation(ctx context.Context, cfg chatConfig) error {
+	return chromedp.Run(ctx,
+		chromedp.WaitVisible(cfg.textBoxSelector, chromedp.ByQuery),
+		chromedp.Click(cfg.textBoxSelector, chromedp.ByQuery),
+		chromedp.SendKeys(cfg.textBoxSelector, cfg.message, chromedp.ByQuery),
+		chromedp.WaitNotPresent(cfg.sendButtonSelector+"[disabled]", chromedp.ByQuery),
+		chromedp.Click(cfg.sendButtonSelector, chromedp.ByQuery),
+	)
+}
+
 func main() {
-	opts := append(
-		chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.ExecPath("/usr/bin/brave"),
-		chromedp.Flag("headless", false),
-		chromedp.Flag(
-			"user-data-dir",
-			"/home/r4ppz/.config/BraveSoftware/Brave-Browser",
-		),
-		chromedp.Flag(
-			"profile-directory",
-			"Default",
-		),
-		chromedp.Flag(
-			"app",
-			"https://chatgpt.com/?temporary-chat=true",
-		),
-		chromedp.Flag("enable-automation", false),
-	)
+	cfg := defaultConfig()
+	allocOpts := buildAllocatorOpts(cfg)
 
-	allocCtx, cancel := chromedp.NewExecAllocator(
-		context.Background(),
-		opts...,
-	)
-	defer cancel()
+	allocCtx, cancelAlloc := chromedp.NewExecAllocator(context.Background(), allocOpts...)
+	defer cancelAlloc()
 
-	ctx, cancel := chromedp.NewContext(allocCtx)
-	defer cancel()
+	ctx, cancelCtx := chromedp.NewContext(allocCtx)
+	defer cancelCtx()
 
-	const (
-		textBox       = `[contenteditable="true"][role="textbox"]`
-		sendButton    = `button[data-testid="send-button"]`
-		messageToSend = `Hello, testing!`
-	)
-
-	err := chromedp.Run(
-		ctx,
-		chromedp.WaitVisible(
-			textBox,
-			chromedp.ByQuery,
-		),
-
-		chromedp.Click(
-			textBox,
-			chromedp.ByQuery,
-		),
-
-		chromedp.SendKeys(
-			textBox,
-			messageToSend,
-			chromedp.ByQuery,
-		),
-
-		chromedp.WaitNotPresent(
-			sendButton+"[disabled]",
-			chromedp.ByQuery,
-		),
-
-		chromedp.Click(
-			sendButton,
-			chromedp.ByQuery,
-		),
-	)
-
-	if err != nil {
-		log.Fatal(err)
+	if err := runAutomation(ctx, cfg); err != nil {
+		log.Fatalf("automation failed: %v", err)
 	}
 
 	// Wait until either the browser is closed or Ctrl+C is pressed.
@@ -86,9 +79,7 @@ func main() {
 	select {
 	case <-ctx.Done():
 		log.Println("Browser closed.")
-
 	case <-sig:
 		log.Println("Ctrl+C received.")
 	}
-
 }
